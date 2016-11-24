390228203f809ef6eafc64298135722999bd7824
Matthieu Freeman <me@ilambda.io>
2016-11-24 20:13:03 +0100

    Edited typo in ps2.c

--- src/ps2.c
+++ src/ps2.c
@@ -1,7 +1,6 @@
 #include "../inc/ps2.h" 
 
-
-void ps2sim_setdata(unsigned char value) {
+void ps2_setdata(unsigned char value) {
   if (value) {
     *data_pin |= 1 << data_bit;
   } else {
@@ -9,7 +8,7 @@ void ps2sim_setdata(unsigned char value) {
   }
 }
 
-void ps2sim_setclock(unsigned char value) {
+void ps2_setclock(unsigned char value) {
   if (value) {
     *clock_pin |= 1 << clock_bit;
   } else {
@@ -18,24 +17,24 @@ void ps2sim_setclock(unsigned char value) {
   __delay_us(5);
 } 
 
-void ps2sim_init(unsigned char* datapin, unsigned char databit, unsigned char* clockpin, unsigned char clockbit) {
+void ps2_init(unsigned char* datapin, unsigned char databit, unsigned char* clockpin, unsigned char clockbit) {
     // gather data_pin and clock_pin
     data_pin = datapin;
     data_bit = databit;
     clock_pin = clockpin;
     clock_bit = clockbit;
     // default them to high
-    ps2sim_setclock(1);
-    ps2sim_setdata(1);
+    ps2_setclock(1);
+    ps2_setdata(1);
 }
 
-void ps2sim_send(unsigned char buffer) {
+void ps2_send(unsigned char buffer) {
   // put the clock and the data down
-  ps2sim_setclock(1);
-  ps2sim_setdata(0);
+  ps2_setclock(1);
+  ps2_setdata(0);
   __delay_us(PS2_CLOCK_LENGTH_HALF);
-  ps2sim_setclock(0);
-  ps2sim_setdata(0);
+  ps2_setclock(0);
+  ps2_setdata(0);
   __delay_us(PS2_CLOCK_LENGTH_HALF);
   
   // start sending bits
@@ -43,35 +42,35 @@ void ps2sim_send(unsigned char buffer) {
   uint8_t parity = 0xFF;
   while (i < 8) {
     // set clock up
-    ps2sim_setclock(1);
+    ps2_setclock(1);
     // getting the right bit
     uint8_t tempbit = buffer & (1 << i);
     parity = tempbit ? ~parity : parity;
-    ps2sim_setdata(tempbit);
+    ps2_setdata(tempbit);
     __delay_us(PS2_CLOCK_LENGTH_HALF);
     // down
-    ps2sim_setclock(0);
+    ps2_setclock(0);
     __delay_us(PS2_CLOCK_LENGTH_HALF);
     // increment
     i++;
   }
 
   // parity bit
-  ps2sim_setclock(1);
-  ps2sim_setdata(parity);
+  ps2_setclock(1);
+  ps2_setdata(parity);
   __delay_us(PS2_CLOCK_LENGTH_HALF);
-  ps2sim_setclock(0);
+  ps2_setclock(0);
   __delay_us(PS2_CLOCK_LENGTH_HALF);
 
   // end
-  ps2sim_setclock(1);
-  ps2sim_setdata(1);
+  ps2_setclock(1);
+  ps2_setdata(1);
   __delay_us(PS2_CLOCK_LENGTH_HALF);
-  ps2sim_setclock(0);
-  ps2sim_setdata(1);
+  ps2_setclock(0);
+  ps2_setdata(1);
   __delay_us(PS2_CLOCK_LENGTH_HALF);
 
   // put line back to high
-  ps2sim_setclock(1);
-  ps2sim_setdata(1);
+  ps2_setclock(1);
+  ps2_setdata(1);
 }
